995ce1e28ea982f7a05eb4df1ad89dfef1cdce16
Joey <170joeychua@gmail.com>
2022-02-16 21:45:55 +0800

    Fix typos

--- LivingLab.Web/Controllers/DashboardController.cs
+++ LivingLab.Web/Controllers/DashboardController.cs
@@ -32,10 +32,10 @@ public class DashboardController : Controller
 
         var builder = new StringBuilder();
         List<Log> Logs = logList();
-        builder.AppendLine("Device SerialNo,Device Type,Toatl Energy Usage,Energy Usage Cost,Energy Usage Intensity");
+        builder.AppendLine("Device SerialNo,Device Type,Total Energy Usage,Energy Usage Cost,Energy Usage Intensity");
         foreach (var log in Logs)
         {
-            builder.AppendLine($"{log.DeviceSerialNo},{log.DeviceType},{log.ToatlEnergyUsage},{log.EnergyUsageCost},{log.EnergyUsageIntensity}");
+            builder.AppendLine($"{log.DeviceSerialNo},{log.DeviceType},{log.TotalEnergyUsage},{log.EnergyUsageCost},{log.EnergyUsageIntensity}");
         }
 
         return File(Encoding.UTF8.GetBytes(builder.ToString()), "text/csv", "Device Energy Usage.csv");
@@ -49,23 +49,23 @@ public class DashboardController : Controller
     }
 
     public struct Log{
-        public Log (String DeviceserialNo, String Devicetype, int Toatlenergyusage, double Energyusagecost, double Energyusageintensity) {
+        public Log (String DeviceserialNo, String Devicetype, int Totalenergyusage, double Energyusagecost, double Energyusageintensity) {
             DeviceSerialNo = DeviceserialNo;
             DeviceType = Devicetype;
-            ToatlEnergyUsage = Toatlenergyusage;
+            TotalEnergyUsage = Totalenergyusage;
             EnergyUsageCost = Energyusagecost;
             EnergyUsageIntensity = Energyusageintensity;
         } 
         public String DeviceSerialNo { get; }
         public String DeviceType { get; }
-        public int ToatlEnergyUsage { get; }
+        public int TotalEnergyUsage { get; }
         public double EnergyUsageCost { get; }
         public double EnergyUsageIntensity { get; }
     }
 
     public List<Log> logList() {
         List<Log> Logs = new List<Log>();
-        Logs.Add(new Log("Sensor-12120","sensor",234,23.21,43.2));
+        Logs.Add(new Log("Sensor-12120","Sensor",234,23.21,43.2));
         Logs.Add(new Log("Actuator-0881","Actuator",121,13.45,21.2));
         Logs.Add(new Log("Robot-73","Robot",691,83.45,21.2));
 
