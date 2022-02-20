b84eeadd9406b3e670f902c606b5c18d45df6b13
Ng Han Yi <nghanyi1997@gmail.com>
2022-02-21 00:57:45 +0800

    #91 use dummy devices serial number

--- LivingLab.DataGenerator/DeviceBuilder.cs
+++ LivingLab.DataGenerator/DeviceBuilder.cs
@@ -7,11 +7,6 @@ namespace LivingLab.DataGenerator;
 /// </summary>
 public static class DeviceBuilder
 {
-    const string SMART_SENSOR_DEVICE_1 = "DEVICE-1598";
-    const string SMART_SENSOR_DEVICE_2 = "DEVICE-3390";
-    const string SMART_LIGHTING_DEVICE_1 = "DEVICE-9987";
-    const string SMART_LIGHTING_DEVICE_2 = "DEVICE-9988";
-    
     /// <summary>
     /// Generates a dictionary and associate each device type and interval.
     /// </summary>
@@ -21,27 +16,27 @@ public static class DeviceBuilder
     {
         return new Dictionary<string, EnergyUsageLog>
         {
-            { SMART_SENSOR_DEVICE_1, new EnergyUsageLog
+            { DummyDevices.SMART_SENSOR_DEVICE_1, new EnergyUsageLog
             {
-                SerialNo = SMART_SENSOR_DEVICE_1,
+                SerialNo = DummyDevices.SMART_SENSOR_DEVICE_1,
                 Interval = 5,
                 DeviceType = DeviceTypes.SMART_SENSOR
             } }, 
-            { SMART_SENSOR_DEVICE_2, new EnergyUsageLog
+            { DummyDevices.SMART_SENSOR_DEVICE_2, new EnergyUsageLog
             {
-                SerialNo = SMART_SENSOR_DEVICE_2,
+                SerialNo = DummyDevices.SMART_SENSOR_DEVICE_2,
                 Interval = 10,
                 DeviceType = DeviceTypes.SMART_SENSOR
             } },
-            { SMART_LIGHTING_DEVICE_1, new EnergyUsageLog
+            { DummyDevices.SMART_LIGHTING_DEVICE_1, new EnergyUsageLog
             {
-                SerialNo = SMART_LIGHTING_DEVICE_1,
+                SerialNo = DummyDevices.SMART_LIGHTING_DEVICE_1,
                 Interval = 10,
                 DeviceType = DeviceTypes.SMART_LIGHTING
             } },
-            { SMART_LIGHTING_DEVICE_2, new EnergyUsageLog
+            { DummyDevices.SMART_LIGHTING_DEVICE_2, new EnergyUsageLog
             {
-                SerialNo = SMART_LIGHTING_DEVICE_2,
+                SerialNo = DummyDevices.SMART_LIGHTING_DEVICE_2,
                 Interval = 15,
                 DeviceType = DeviceTypes.SMART_LIGHTING
             } }
