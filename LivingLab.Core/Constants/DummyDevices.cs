f99b640b3b73d1caa7a7ef00c8d109c702138869
Ng Han Yi <nghanyi1997@gmail.com>
2022-02-21 00:56:49 +0800

    #91 create dummy devices serial number

---
+++ LivingLab.Core/Constants/DummyDevices.cs
@@ -0,0 +1,9 @@
+namespace LivingLab.Core.Constants;
+
+public static class DummyDevices
+{
+    public const string SMART_SENSOR_DEVICE_1 = "DEVICE-1598";
+    public const string SMART_SENSOR_DEVICE_2 = "DEVICE-3390";
+    public const string SMART_LIGHTING_DEVICE_1 = "DEVICE-9987";
+    public const string SMART_LIGHTING_DEVICE_2 = "DEVICE-9988";
+}
