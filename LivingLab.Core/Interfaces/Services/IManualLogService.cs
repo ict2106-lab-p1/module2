001ecca9163251dc3e8df3cba966c1fbc379e076
Ng Han Yi <nghanyi1997@gmail.com>
2022-03-09 22:21:44 +0800

    #147 fix: typo

--- LivingLab.Core/Interfaces/Services/IManualLogService.cs
+++ LivingLab.Core/Interfaces/Services/IManualLogService.cs
@@ -5,6 +5,6 @@ namespace LivingLab.Core.Interfaces.Services;
 public interface IManualLogService
 {
     void Process(IFormFile file);
-    void Archieve(int deviceId, DateTime start, DateTime end);
-    void Archieve(string deviceType, DateTime start, DateTime end);
+    void Archive(int deviceId, DateTime start, DateTime end);
+    void Archive(string deviceType, DateTime start, DateTime end);
 }
