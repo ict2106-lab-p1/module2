920fdd6e5ef1b6e3b54a64f256526fb00a675d68
HongYing222 <72849652+HongYing222@users.noreply.github.com>
2022-03-17 15:19:19 +0800

    dual-axis graph and validation

--- LivingLab.Web/Controllers/EnergyUsageComparisonController.cs
+++ LivingLab.Web/Controllers/EnergyUsageComparisonController.cs
@@ -7,7 +7,8 @@ using LivingLab.Core.Interfaces.Repositories;
 using LivingLab.Core.Entities;
 using LivingLab.Web.Models.ViewModels;
 using LivingLab.Web.UIServices.EnergyUsage;
-
+using System.Data;
+using System.Text.RegularExpressions;
 
 namespace LivingLab.Web.Controllers;
 /// <remarks>
@@ -36,4 +37,88 @@ public class EnergyUsageComparisonController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+
+    [HttpGet]
+    public string[] GetType(string compareType)
+    {
+        if (compareType == "DeviceType")
+        {
+            //need to get from db
+            string[] array = { "Light", "Robot", "Microprocessor", "VR Device" };
+            return array;
+        }
+        else
+        {
+            //need to get from db
+            string[] array = { "SR6H", "LT4A", "SR5G", "SR7A" };
+            return array;
+        }
+        
+    }
+
+
+
+    //need to add bench-mark for the lab graph
+    [HttpPost]
+    public JsonResult GetGraph(string type, string compareFactor)
+    {
+        //string compareType = type.Substring(1, type.Length - 2);
+        string compareType = Regex.Replace(type, @"[^A-Z^a-z]+", String.Empty);
+        string[] compareFactorArray = compareFactor.Split(",");
+
+        List<object> iData = new List<object>();
+        //Creating sample data  
+        DataTable dt = new DataTable();
+        dt.Columns.Add(compareType, System.Type.GetType("System.String"));
+        dt.Columns.Add("Energy Usage", System.Type.GetType("System.Int32"));
+        dt.Columns.Add("Energy Intensity", System.Type.GetType("System.Int32"));
+        dt.Columns.Add("Benchmark", System.Type.GetType("System.Int32"));//for energy usage
+
+        DataRow dr = dt.NewRow();
+
+        for (int i = 0; i < compareFactorArray.Length; i++)
+        {
+            compareFactorArray[i] = Regex.Replace(compareFactorArray[i], @"[^^a-zA-Z0-9 -]+", String.Empty);
+            if (i == 0)
+            {
+                dr[compareType] = compareFactorArray[i];
+                dr["Energy Usage"] = 213; //need to get from db
+                dr["Energy Intensity"] = 123; //need to get from db
+                dr["Benchmark"] = 120;//need to get from db
+                dt.Rows.Add(dr);
+            }
+            else
+            {
+                dr = dt.NewRow();
+                dr[compareType] = compareFactorArray[i];
+                dr["Energy Usage"] = 200; //need to get from db
+                dr["Energy Intensity"] = 456; //need to get from db
+                dr["Benchmark"] = 120;//need to get from db
+                dt.Rows.Add(dr);
+            }
+        }
+
+        foreach (DataColumn dc in dt.Columns)
+        {
+            List<object> x = new List<object>();
+            x = (from DataRow drr in dt.Rows select drr[dc.ColumnName]).ToList();
+            iData.Add(x);
+        }
+
+        return Json(iData);
+    }
+
+
+
+    //public GetLabEnergyUsageDetail(int labId)
+    //{
+
+    //    return true;
+    //}
+
+    //public GetDeviceEnergy()
+    //{
+
+    //}
 }
---
+++ LivingLab.Web/wwwroot/js/energyUsageComparison.js
@@ -0,0 +1,223 @@
+﻿
+var allArray = [];
+function selectComparisonType() {
+
+    var type = $("#compare :selected").val();
+    console.log(type);
+
+    if (type === "DeviceType") {
+        for (var i = 1; i <= 4; i++) {
+            $("#opt" + i).text("Select Device " + i + ":");
+        }
+    }
+    else {
+        for (var i = 1; i <= 4; i++) {
+            $("#opt" + i).text("Select Lab " + i + ":");
+        }
+    }
+
+    for (var i = 1; i <= 4; i++) {
+        $("#option" + i).empty();
+    }
+
+
+    $.ajax({
+        url: "/EnergyUsageComparison/GetType",
+        type: "GET",
+        data: { "compareType": type },
+        success: function (response) {
+            console.log(response);
+            for (var i = 1; i <= 4; i++) {
+                $("#option" + i).append($("<option></option>")
+                    .attr("value", "")
+                    .text("Please Select"));
+            }
+            for (var i = 0; i < response.length; i++) {
+                for (var j = 1; j <= 4; j++) {
+                    $("#option" + j).append($("<option></option>")
+                        .attr("value", response[i])
+                        .text(response[i]));
+                }
+                allArray.push(response[i]);
+            }
+
+        },
+        error: function (response) {
+            console.log(response);
+        }
+    });
+}
+
+$(document).ready(function () {
+    selectComparisonType()
+
+})
+
+
+
+function removeChosenType() {
+    var ddlSelevted = event.target.id;
+    var selected = $("#" + ddlSelevted + " :selected").val();
+    var selectedArray = [];
+    console.log(ddlSelevted.substring(ddlSelevted.length - 1, ddlSelevted.length));
+    //if (selected != "") {
+        for (var x = 1; x <= 4; x++) {
+            if (x != ddlSelevted.substring(ddlSelevted.length - 1, ddlSelevted.length)) {
+                if (selected != "") {
+                    $("#option" + x + " option[value = '" + selected + "']").hide();
+                }
+            }
+            var checkAllOptionSelected = $("#option" + x + " :selected").val();
+            if (checkAllOptionSelected != "") {
+                selectedArray.push(checkAllOptionSelected);
+            }
+        }
+    //}
+    var difference = allArray.filter(x => !selectedArray.includes(x));
+    for (var j = 0; j < difference.length; j++) {
+        for (var x = 1; x <= 4; x++) {
+            $("#option" + x + " option[value = '" + difference[j] + "']").show();
+
+        }
+    }
+}
+
+function toggleModal() {
+    document.getElementById("errormodal" + "-backdrop").classList.toggle("hidden");
+    document.getElementById("errormodal").classList.toggle("hidden");
+    document.getElementById("errormodal" + "-backdrop").classList.toggle("flex");
+    document.getElementById("errormodal").classList.toggle("flex");
+}
+
+function compareData() {
+
+
+    var type = $("#compare :selected").val();
+
+    var num = 0;
+    const compareFactor = [];
+    for (var i = 1; i <= 4; i++) {
+        if ($("#option" + i + " :selected").val() === "") {
+            num += 1;
+        }
+        else {
+            compareFactor.push($("#option" + i + " :selected").val());
+        }
+    }
+    console.log("Seeeeee: " + compareFactor);
+
+    if (num > 2) {
+        toggleModal()
+    }
+    else {
+        if (type != "DeviceType") {
+            $.ajax({
+                url: "/EnergyUsageComparison/GetGraph",
+                type: "POST",
+                data: { "type": JSON.stringify(type), "compareFactor": JSON.stringify(compareFactor) },
+                success: function (response) {
+                    var aData = response;
+                    var aLabels = aData[0];
+                    var aDatasets1 = aData[1];
+                    var aDatasets2 = aData[2];
+
+                    //var benchmark = aData[3][0];
+
+                    console.log("Data for benchmark: " + aData[3]);
+                    console.log("Benchmark: " + aData[3][0]);
+
+
+                    const data = {
+                        labels: aLabels,
+                        datasets: [
+                            {
+                                label: 'Energy Usage',
+                                data: aDatasets1,
+                                borderColor: "blue",
+                                backgroundColor: "rgba(66, 134, 244, 0.1)",
+                                order: 0
+                            },
+                            {
+                                label: 'Energy Intensity',
+                                data: aDatasets2,
+                                borderColor: 'rgb(255, 99, 132)',
+                                backgroundColor: 'transparent',
+                                type: 'line',
+                                order: 1
+                            }
+                        ]
+                    };
+
+                    var ctx = $("#graph").get(0).getContext("2d");
+
+
+                    var options = {
+                        legend: {
+                            display: true,
+                        },
+                        tooltips: {
+                            enabled: false,
+                        },
+                        scales: {
+                            xAxes: [{
+                                display: true,
+                                ticks: {
+                                    beginAtZero: true
+                                },
+                            }],
+                            yAxes: [{
+                                display: true,
+                                ticks: {
+                                    beginAtZero: true
+                                },
+                            }]
+                        },
+                        annotation: {
+                            annotations: [{
+                                type: 'line',
+                                mode: 'horizontal',
+                                scaleID: 'y-axis-0',
+                                value: '26',
+                                borderColor: 'tomato',
+                                borderWidth: 1
+                            }],
+                            drawTime: "afterDraw" // (default)
+                        }
+                    };
+
+                    var myNewChart = new Chart(ctx, {
+                        type: 'bar',
+                        data: data,
+                        options: {
+                            responsive: true,
+                            title: { display: true, text: 'Energy Usage and Energy Intensity' },
+                            legend: { position: 'bottom' },
+                            scales: {
+                                xAxes: [{ gridLines: { display: false }, display: true, scaleLabel: { display: false, labelString: '' } }],
+                                yAxes: [{ gridLines: { display: false }, display: true, scaleLabel: { display: false, labelString: '' }, ticks: { stepSize: 50, beginAtZero: true } }]
+                            },
+                            tooltips: {
+                                mode: 'index',
+                                intersect: true,
+                                enabled: false
+                            },
+                            annotation: {
+                                annotations: [{
+                                    type: 'line',
+                                    mode: 'horizontal',
+                                    scaleID: 'y-axis-0',
+                                    value: '26',
+                                    borderColor: 'tomato',
+                                    borderWidth: 1
+                                }],
+                                drawTime: "afterDraw" // (default)
+                            }
+                        }
+                    });
+                }
+            });
+        }
+        
+    }
+}
+
