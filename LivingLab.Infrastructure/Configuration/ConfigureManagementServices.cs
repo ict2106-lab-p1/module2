35d5d1fa683dba4d5b4ea3ea29c35e369ffcb8ee
Ng Han Yi <nghanyi1997@gmail.com>
2022-03-19 14:32:51 +0800

    #207 organise core services configurations

---
+++ LivingLab.Infrastructure/Configuration/ConfigureManagementServices.cs
@@ -0,0 +1,47 @@
+using LivingLab.Core.DomainServices;
+using LivingLab.Core.Interfaces.Repositories;
+using LivingLab.Core.Interfaces.Services;
+using LivingLab.Infrastructure.Repositories;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LivingLab.Infrastructure.Configuration;
+
+public static class ConfigureManagementServices
+{
+    public static IServiceCollection AddManagementServices(this IServiceCollection services)
+    {
+        AddTransientServices(services);
+        AddScopedServices(services);
+        AddSingletonServices(services);
+        return services;
+    }
+
+    private static IServiceCollection AddTransientServices(this IServiceCollection services)
+    {
+        // Repositories
+        services.AddTransient<IAccessoryRepository, AccessoryRepository>();
+        services.AddTransient<IAccessoryTypeRepository, AccessoryTypeRepository>();
+        services.AddTransient<IDeviceRepository, DeviceRepository>();
+        services.AddTransient<ISessionStatsRepository, SessionStatsRepository>();
+
+        // Services
+        services.AddTransient<IDeviceDomainService, DeviceDomainService>();
+        services.AddTransient<IAccessoryDomainService, AccessoryDomainService>();
+        
+        return services;
+    }
+
+    private static IServiceCollection AddScopedServices(this IServiceCollection services)
+    {
+        // services.AddScoped<ITodoRepository, TodoRepository>();
+
+        return services;
+    }
+    private static IServiceCollection AddSingletonServices(this IServiceCollection services)
+    {
+        // services.AddSingleton<ITodoRepository, TodoRepository>();
+
+        return services;
+    }
+}
