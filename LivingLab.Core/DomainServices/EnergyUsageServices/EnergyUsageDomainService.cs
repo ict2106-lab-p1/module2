d4b644fe4ceb3867feb79c5a0537afb7c56d70b9
Ng Han Yi <nghanyi1997@gmail.com>
2022-03-20 00:55:33 +0800

    #205 add todos

--- LivingLab.Core/DomainServices/EnergyUsageServices/EnergyUsageDomainService.cs
+++ LivingLab.Core/DomainServices/EnergyUsageServices/EnergyUsageDomainService.cs
@@ -9,22 +9,25 @@ namespace LivingLab.Core.DomainServices.EnergyUsageServices;
 /// </remarks>
 public class EnergyUsageDomainService : IEnergyUsageDomainService
 {
-    private readonly ILabRepository _lapRepository;
+    private readonly ILabRepository _labRepository;
     private readonly IEnergyUsageRepository _energyUsageRepository;
     
-    public EnergyUsageDomainService(ILabRepository lapRepository, IEnergyUsageRepository energyUsageRepository)
+    public EnergyUsageDomainService(ILabRepository labRepository, IEnergyUsageRepository energyUsageRepository)
     {
-        _lapRepository = lapRepository;
+        _labRepository = labRepository;
         _energyUsageRepository = energyUsageRepository;
     }
     
     public Task<List<EnergyUsageDTO>> GetEnergyUsage(EnergyUsageFilterDTO filter)
     {
+        // TODO: Call Energy Usage repo to get filtered energy usage
+        // TODO: Map to DTO
         throw new NotImplementedException();
     }
 
     public Task SetLabEnergyBenchmark(EnergyBenchmarkDTO benchmark)
     {
+        // TODO: Call Lab repo to set energy benchmark
         throw new NotImplementedException();
     }
 }
